dd030cd9b9f9fb5162cc73933e9b8d7d564406a3
Shauna-Bromley <166406107+Shauna-Bromley@users.noreply.github.com>
2024-10-23 21:13:12 +0100

    fixed animations added jump

--- CatPlatformer/Assets/Scripts/PlayerController.cs
+++ CatPlatformer/Assets/Scripts/PlayerController.cs
@@ -6,30 +6,67 @@ public class PlayerController : MonoBehaviour
 {
     public int direction = 0;
     public int speed;
+    public float JumpHeight;
+    public bool isJumping = false;
     Animator animator;
+    Rigidbody2D rigidbody2D;
     // Start is called before the first frame update
     void Start()
     {
         animator = GetComponent<Animator>();
+        rigidbody2D = GetComponent<Rigidbody2D>();
+        animator.SetFloat("Move X",0);
+        animator.SetFloat("Move Y",1);
     }
 
     // Update is called once per frame
     void Update()
     {
         Vector2 position = transform.position;
-        float move = Input.GetAxis("Horizontal");
-        position.x = position.x + speed * Time.deltaTime * move;
+        float moveBy = Input.GetAxis("Horizontal");
+        position.x = position.x + speed * moveBy * Time.deltaTime;
         transform.position = position;
-
-        if (move != 0)
+        playAnimation(moveBy);
+        if (!isJumping && Input.GetKeyDown(KeyCode.Space))
         {
-            direction = move < 0 ? -1 : 1;
-            animator.SetFloat("Move X",direction);
+            isJumping = true;
+            rigidbody2D.velocity = Vector3.zero;
+            rigidbody2D.AddForce(new Vector2(0, Mathf.Sqrt(-2 * Physics2D.gravity.y * JumpHeight)), ForceMode2D.Impulse);
+
+        }
+        if (moveBy != 0)
+        {
+            direction = moveBy < 0 ? -1 : 1;
              
+        }
+        
+    }
+
+    private void playAnimation(float moveBy)
+    {
+        if (moveBy == 0)
+        {
+            animator.SetFloat("Move X", 0);
+
+        }
+        else if (moveBy < 0)
+        {
+            animator.SetFloat("Move X", -1);
+            animator.SetFloat("Move Y", -1);
         }
         else
         {
-            animator.SetFloat("Move X", 0);
+            animator.SetFloat("Move X", 1);
+            animator.SetFloat("Move Y", 1);
+        }
+
+    }
+
+    private void OnCollisionEnter2D(Collision2D col)
+    {
+        if (isJumping)
+        {
+            isJumping = false;
         }
     }
 }
