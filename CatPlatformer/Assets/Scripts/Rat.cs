e20eab3747833caef72f3f3c9ca9c07592f9da94
Shauna-Bromley <166406107+Shauna-Bromley@users.noreply.github.com>
2024-10-23 22:26:33 +0100

    added a menu scene

---
+++ CatPlatformer/Assets/Scripts/Rat.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class Rat : MonoBehaviour
+{
+    [SerializeField] PlayerController player;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnCollisionEnter2D(Collision2D other)
+    {
+        if (other.gameObject.tag == "Player" && this.tag == "Rat")
+        {
+            Destroy(this.gameObject);
+            //player.AddLife();
+        }
+    }
+}
