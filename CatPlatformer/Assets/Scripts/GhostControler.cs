bb0583134173909d03323019072ca547b12f584c
Shauna-Bromley <166406107+Shauna-Bromley@users.noreply.github.com>
2024-10-28 15:09:48 +0000

    added rigidbody and collider to ghost

--- CatPlatformer/Assets/Scripts/GhostControler.cs
+++ CatPlatformer/Assets/Scripts/GhostControler.cs
@@ -4,15 +4,33 @@ using UnityEngine;
 
 public class GhostControler : MonoBehaviour
 {
+    Animator animator;
+    public float distanceTime;
+    public float speed;
+    float direction = 1;
+    float timeInDirection;
     // Start is called before the first frame update
     void Start()
     {
+        animator = GetComponent<Animator>();
+        timeInDirection = distanceTime;
+        animator.SetFloat("Move X",direction);
         
     }
 
     // Update is called once per frame
     void Update()
     {
+        Vector2 position = transform.position;
+        position.x = position.x + speed * Time.deltaTime * direction;
+        transform.position = position;
+        timeInDirection -= Time.deltaTime;
+        if (timeInDirection < 0 )
+        {
+            direction *= -1;
+            timeInDirection = distanceTime;
+            animator.SetFloat("Move X", direction);
+        }
         
     }
 }
